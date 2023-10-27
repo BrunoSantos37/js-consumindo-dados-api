6663b4b5d3031dc52f74bc3b9a375c96221db53a
Bruno Candido dos Santos <111137962+BrunoSantos37@users.noreply.github.com>
2023-10-27 18:18:22 -0300

    Add meu script

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
-    mensagemErro.innerHTML = "";
+    const mensagemErro = document.querySelector('#erro');
+    mensagemErro.innerHTML = '';
     try {
-        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        var consultaCEPConvertida = await consultaCEP.json();
-        if (consultaCEPConvertida.erro) {
-            throw Error('CEP não existente!');
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var cepConvertido = await consultaCep.json(); // cepConvertido receberá um objeto json
+        // erro = true, quando CEP é válido, mas não é existente
+        if (cepConvertido.erro) {
+            throw Error ('Cep inexistente')
+            mensagemErro.innerHTML = `<p>CEP : ${cep} inválido! Tente Novamente.</p>`;
+        } else {
+            const inputCidade = document.querySelector('#cidade');
+            const inputRua = document.querySelector('#bairro');
+            const inputEstado = document.querySelector('#estado');
+            const inputBairro = document.querySelector('#bairro');
+
+            inputCidade.value = cepConvertido.localidade;
+            inputRua.value = cepConvertido.logradouro;
+            inputEstado.value = cepConvertido.uf;
+            inputBairro.value = cepConvertido.bairro;
         }
-        var cidade = document.getElementById('cidade');
-        var logradouro = document.getElementById('endereco');
-        var estado = document.getElementById('estado');
 
-        cidade.value = consultaCEPConvertida.localidade;
-        logradouro.value = consultaCEPConvertida.logradouro;
-        estado.value = consultaCEPConvertida.uf;
-
-        console.log(consultaCEPConvertida);
-        return consultaCEPConvertida;
     } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
+        mensagemErro.innerHTML = `<p>CEP : ${cep} inválido! Tente Novamente.</p>`;
         console.log(erro);
     }
 }
 
-var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+const inputCep = document.querySelector('#cep');
+
+inputCep.addEventListener('focusout', () => {
+    buscaEndereco(inputCep.value);
+
+})
+
